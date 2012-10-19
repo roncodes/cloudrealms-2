3d8383636c463630c94644a257f1a33b39a575c3
Ronald A. Richardson <theprestig3@gmail.com>
2012-10-19 15:48:39 -0700

    index all characters via data table in editor

--- application/views/editor/characters/index.php
+++ application/views/editor/characters/index.php
@@ -14,6 +14,37 @@
 			<div class="page-header">
 				<h1>All Characters</h1>
 			</div>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>User</th>
+						<th>Name</th>
+						<th>Level</th>
+						<th>Gold</th>
+						<th>Class</th>
+						<th>Gender</th>
+						<th>Zodiac</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+				<?php foreach ($characters as $character): ?>
+					<tr>
+						<td><?=$character->user_id?></td>
+						<td><?=$character->name?></td>
+						<td><?=$character->level?></td>
+						<td><?=$character->gold?></td>
+						<td><?=$character->class?></td>
+						<td><?=$character->gender?></td>
+						<td><?=$character->zodiac?></td>
+						<td>
+							<a href="<?=base_url('editor/characters/edit/'.$character->id)?>"><i class="icon-pencil"></i></a>
+							<a href="<?=base_url('editor/characters/delete/'.$character->id)?>"><i class="icon-trash"></i></a>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </div>
