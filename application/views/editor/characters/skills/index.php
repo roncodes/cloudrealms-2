378ae4e2ef7722c761befc46b0b2df5f886833c3
Ronald A. Richardson <theprestig3@gmail.com>
2012-10-25 03:44:34 -0700

    updated db and added info block for skills

--- application/views/editor/characters/skills/index.php
+++ application/views/editor/characters/skills/index.php
@@ -1,7 +1,10 @@
 <div class="pane">
 	<div class="page-header">
 		<a href="<?=base_url('editor/characters/skills/create')?>" class="btn pull-right">Create Skill</a>
+		<a href="javascript:$('.info').slideToggle();" class="btn btn-info pull-right"><i class="icon-white icon-info-sign" style="margin-top:3px;"></i> Toggle Info</a>
 		<h1>All Skills</h1>
+		<p class="info" style="display:none;">Skills represent actions that can be taken in game. 
+		Skill points are a measure of how proficient the player is at these skills and increasing them grants benefits to the skill as well as granting access to perks that may be taken upon leveling up.</p>
 	</div>
 	<table class="table">
 		<thead>
